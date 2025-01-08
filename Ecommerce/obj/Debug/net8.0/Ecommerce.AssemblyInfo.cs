e9c9bcebc0db404bcf21ca74b08d54ebfbef9e2b
Adesh Pandey <adesh.pandey1673@gmail.com>
2025-01-08 21:01:09 +0545

    frontend added

--- Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
+++ Ecommerce/obj/Debug/net8.0/Ecommerce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5578c2eccfc72b5248c6278139f754032633cbdb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faa9ec538e0a98ad2a6d2a9f5905b6dd0f38e04e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
